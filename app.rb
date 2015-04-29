ab23ade2aca3197ba870fbd1cf8f50ac3d104fa1
Jesse James & Desire Perrault-Reid <student@epicodus.com>
2015-04-29 14:13:11 -0700

    app.rb file updated

---
+++ app.rb
@@ -0,0 +1,50 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('.lib/vehicle')
+require('.lib/dealership')
+
+get('/') do
+  erb(:index)
+end
+
+get('/dealerships/new') do
+  erb(:dealerships_form)
+end
+
+get('/dealerships') do
+  erb(:dealerships)
+end
+
+post('/dealerships') do
+  @dealerships = Dealership.all()
+  Dealership.new(name).save()
+  @dealerships = Dealership.all()
+  erb(:success)
+end
+
+get('/vehicles/:id') do
+  @vehicle = Vehicle.find(params.fetch('id'))
+  erb(:vehicle)
+end
+
+get('/dealerships/:id') do
+  @dealership = Dealership.find(params.fetch('id').to_i())
+  erb(:dealership)
+end
+
+get('/dealerships/:id/vehicles/new') do
+  @dealership = Dealership.find(params.fetch('id').to_i())
+  erb(:dealership)
+end
+
+post('/vehicles') do
+  make = params.fetch('make')
+  model = params.fetch('model')
+  year = params.fetch('year')
+  @vehicle = Vehicle.new(make, model, year)
+  @vehicle.save()
+  @dealership = Dealership.find(params.fetch('dealership_id').to_i())
+  @dealership.add_vehicle(@vehicle)
+  erb(:success)
+end
